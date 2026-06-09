9f4817236d68976dada2594a7c500eececd4f0d5
Nguyen Huu Quan <nguyenhuuquan26052005@gmail.com>
2026-06-09 21:53:31 +0700

    Add contact page UI

---
+++ OnlineAuction/wwwroot/js/contact.js
@@ -0,0 +1,52 @@
+﻿document
+    .getElementById("contactForm")
+    .addEventListener("submit", function (e) {
+
+        e.preventDefault();
+
+        let isValid = true;
+
+        const name = document.getElementById("fullName");
+        const email = document.getElementById("email");
+        const message = document.getElementById("message");
+
+        document.getElementById("nameError").innerText = "";
+        document.getElementById("emailError").innerText = "";
+        document.getElementById("messageError").innerText = "";
+
+        if (name.value.trim() === "") {
+            document.getElementById("nameError").innerText =
+                "This field is required";
+            isValid = false;
+        }
+
+        if (email.value.trim() === "") {
+            document.getElementById("emailError").innerText =
+                "This field is required";
+            isValid = false;
+        }
+        else {
+
+            const regex =
+                /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+            if (!regex.test(email.value)) {
+
+                document.getElementById("emailError").innerText =
+                    "Invalid email format";
+
+                isValid = false;
+            }
+        }
+
+        if (message.value.trim() === "") {
+            document.getElementById("messageError").innerText =
+                "This field is required";
+            isValid = false;
+        }
+
+        if (isValid) {
+            alert("Message sent successfully!");
+        }
+
+    });
