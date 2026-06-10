fa966f480acb8cace413d51bfac17db141c83af8
Nguyen Huu Quan <nguyenhuuquan26052005@gmail.com>
2026-06-10 15:10:20 +0700

    page-contact

--- OnlineAuction/obj/Debug/net8.0/OnlineAuction.AssemblyInfo.cs
+++ OnlineAuction/obj/Debug/net8.0/OnlineAuction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+663e164cc5e5f03b877788885c56debade9891f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25e5dff3dec7f6c792ce12f46e3fdfad8f52e808")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
