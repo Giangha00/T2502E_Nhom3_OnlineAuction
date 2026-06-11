cc0f9d497d016ccb12be6e5c677f00f575e0965a
azhai4123 <104208096+azhai4123@users.noreply.github.com>
2026-06-11 17:55:43 +0700

    merge

--- OnlineAuction/obj/Debug/net8.0/OnlineAuction.AssemblyInfo.cs
+++ OnlineAuction/obj/Debug/net8.0/OnlineAuction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346fdd9132765b9378561988d83a1e44aae8dd78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d92ac83c865fe62669f77804d1cae046d079b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
