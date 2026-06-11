f069e7d995f42b22eabc65f064d6b59840faa648
azhai4123 <104208096+azhai4123@users.noreply.github.com>
2026-06-11 17:39:21 +0700

    create view Multi-language

--- OnlineAuction/Program.cs
+++ OnlineAuction/Program.cs
@@ -1,8 +1,36 @@
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddLocalization(options =>
+{
+    options.ResourcesPath = "Resources";
+});
 
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[]
+    {
+        new CultureInfo("en-US"),
+        new CultureInfo("vi-VN"),
+        new CultureInfo("ja-JP"),
+        new CultureInfo("ko-KR")
+    };
+
+    options.DefaultRequestCulture = new RequestCulture("en-US");
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+    
+    // ✅ FIX: Replace default providers with Cookie provider
+    options.RequestCultureProviders.Clear();
+    options.RequestCultureProviders.Add(new QueryStringRequestCultureProvider());
+    options.RequestCultureProviders.Add(new CookieRequestCultureProvider 
+    { 
+        CookieName = CookieRequestCultureProvider.DefaultCookieName
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +43,12 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+// ✅ FIX: RequestLocalization phải được đặt trước Routing
+var localizationOptions = app.Services
+    .GetRequiredService<Microsoft.Extensions.Options.IOptions<RequestLocalizationOptions>>()
+    .Value;
+app.UseRequestLocalization(localizationOptions);
 
 app.UseRouting();
 
--- OnlineAuction/obj/Debug/net8.0/OnlineAuction.AssemblyInfo.cs
+++ OnlineAuction/obj/Debug/net8.0/OnlineAuction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84c0edb3b05f7a3a21ce5fb77f4fced97b91aa9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+346fdd9132765b9378561988d83a1e44aae8dd78")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
