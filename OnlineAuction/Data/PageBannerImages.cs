cd6c68c6fb7234bcbe10f1bedabc6a8df6865dc8
Ha <gianghacd@gmail.com>
2026-06-12 13:20:21 +0700

    update view

---
+++ OnlineAuction/Data/PageBannerImages.cs
@@ -0,0 +1,25 @@
+namespace OnlineAuction.Data;
+
+public static class PageBannerImages
+{
+    public const string Home =
+        "https://images.unsplash.com/photo-1515562141207-7a88fb7ce338?w=1600&h=560&fit=crop&q=80";
+
+    public const string Auction =
+        "https://images.unsplash.com/photo-1503376780353-7e6692767b70?w=1600&h=560&fit=crop&q=80";
+
+    public const string Sell =
+        "https://images.unsplash.com/photo-1556742049-0cfed4f6a45d?w=1600&h=560&fit=crop&q=80";
+
+    public const string About =
+        "https://images.unsplash.com/photo-1560472354-b33ff0c44a43?w=1600&h=560&fit=crop&q=80";
+
+    public const string Contact =
+        "https://images.unsplash.com/photo-1521791136064-7986c2920216?w=1600&h=560&fit=crop&q=80";
+
+    public const string Faq =
+        "https://images.unsplash.com/photo-1516321318423-f06f85e504b3?w=1600&h=560&fit=crop&q=80";
+
+    public const string Policy =
+        "https://images.unsplash.com/photo-1450101499163-c8848c66ca85?w=1600&h=560&fit=crop&q=80";
+}
--- OnlineAuction/obj/Debug/net8.0/OnlineAuction.AssemblyInfo.cs
+++ OnlineAuction/obj/Debug/net8.0/OnlineAuction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b18f142ef670d1f4efcfb2ad38db5e72ac04c92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed6e42b3d011ca26b64f410cd3204632f190195f")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlineAuction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
